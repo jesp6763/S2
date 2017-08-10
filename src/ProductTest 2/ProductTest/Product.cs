d12bebd1ba4968f338dcf05df048a72eced723c7
jesp6763 <jesp6763@edu.campusvejle.dk>
2017-08-10 11:57:23 +0200

    Add files via upload

---
+++ src/ProductTest 2/ProductTest/Product.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ProductTest
+{
+    public class Product
+    {
+        private string name; // The name of the product
+        private string description; // The product description
+        private double price; // The price of the product
+        private int stockAmount; // How much stock is available
+
+        public Product(string name, string description, double price, int stockAmount)
+        {
+            PriceHistory = new PriceHistory();
+            Name = name;
+            Description = description;
+            Price = price;
+            StockAmount = stockAmount;
+        }
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (Regex.IsMatch(value, @"^[ A-Za-z0-9&]+$"))
+                {
+                    if (value.Length <= 30)
+                    {
+                        name = value;
+                    }
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException();
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+        }
+        public string Description
+        {
+            get => description;
+            set
+            {
+                if (value.Length <= 500)
+                {
+                    description = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+        public double Price
+        {
+            get => price;
+            set
+            {
+                if (value >= 0)
+                {
+                    PriceHistory.CommitPrice(value);
+                    price = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+        public int StockAmount
+        {
+            get => stockAmount;
+            set
+            {
+                if (stockAmount >= 0)
+                {
+                    stockAmount = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+        public PriceHistory PriceHistory { get; }
+    }
+}
---
+++ src/ProductTest 2/ProductTest/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ProductTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product("Laptop XL", "Crazy fast laptop!", 19431, 3);
+            Console.WriteLine($"Name: {product1.Name}\nDescription: {product1.Description}\nPrice: {product1.Price}\nStock available: {product1.StockAmount}");
+
+            Seperate();
+            Console.WriteLine($"Changes done to product: {product1.Name}");
+            product1.Price = 23000;
+            Console.WriteLine($"New price: {product1.Price}");
+            product1.Price = 200;
+            Console.WriteLine($"New price: {product1.Price}");
+            Console.WriteLine($"Highest price registeret: {product1.PriceHistory.HighestPrice}");
+            Console.WriteLine($"Lowest price registeret: {product1.PriceHistory.LowestPrice}");
+            product1.Price = 35;
+            Console.WriteLine($"New lowest price registeret: {product1.PriceHistory.LowestPrice}");
+
+            Seperate();
+
+            // Print price history at date
+            DateTime date = DateTime.Today;
+            double[] prices = product1.PriceHistory.GetHistoryAtDate(date);
+            if(prices.Length > 0)
+            {
+                Console.WriteLine($"Price history from: {date.ToLongDateString()}");
+
+                for(int i = 0; i < prices.Length; i++)
+                {
+                    Console.WriteLine(prices[i].ToString("C0"));
+                }
+            }
+            Console.ReadKey();
+        }
+
+        private static void Seperate()
+        {
+            for(int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.Write("=");
+            }
+        }
+    }
+}
